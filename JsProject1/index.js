5b9d7265bb0df3d3c66709b887cf110e3d8560f3
Ibrahimkhan461000 <khan.ibrahim46100@gmail.com>
2026-04-19 12:10:57 +0530

    JS Code

---
+++ JsProject1/index.js
@@ -0,0 +1,23 @@
+// let firstName = "Ibrahim"
+// let lastName = "Khan"
+
+// let fullName = firstName + " " + lastName
+// console.log(fullName)
+
+let myPoints = 3
+
+function add3Points(){
+    myPoints += 3
+}
+
+function removelPoints(){
+    myPoints -= 1
+}
+
+add3Points()
+add3Points()
+add3Points()
+removelPoints()
+removelPoints()
+
+console.log(myPoints)
