dda780124505ac71a065e5fd68fe4c7241a07d33
Ibrahimkhan461000 <khan.ibrahim46100@gmail.com>
2026-04-23 16:21:57 +0530

    JS Code

---
+++ JsProject5/index.js
@@ -0,0 +1,36 @@
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  renderLeads();
+  inputEl.value = "";
+});
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    // listItems +=
+    //   "<li><a target='_blank' href='" +
+    //   myLeads[i] +
+    //   "'>" +
+    //   myLeads[i] +
+    //   "</a></li>";
+
+    listItems += `<li><a target='_blank' href='${myLeads[i]}'>${myLeads[i]}
+      </a></li>`;
+  }
+  ulEl.innerHTML = listItems;
+}
+
+// function renderLead() {
+//   let listItem =
+//     "<li><a target = '_blank' href = '" +
+//     myLeads[i] +
+//     "'>" +
+//     inputEl.value +
+//     "</a></li>";
+//   ulEl.innerHTML += listItem;
+// }
